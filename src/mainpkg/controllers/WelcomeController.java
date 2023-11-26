17507c6a8c7bf09d2be7c32fd27944d488c9b512
dewanwakilahmed <dewanwakilahmed@gmail.com>
2023-11-26 09:06:03 +0600

    Created Welcome Controller

---
+++ src/mainpkg/controllers/WelcomeController.java
@@ -0,0 +1,28 @@
+package mainpkg.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author dewan
+ */
+public class WelcomeController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    private void loadAuthView(ActionEvent event) {
+    }
+
+}
