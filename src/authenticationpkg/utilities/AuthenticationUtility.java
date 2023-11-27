8dbd768048a4120a29c7908dc0f07b0566fccdb8
dewanwakilahmed <dewanwakilahmed@gmail.com>
2023-11-27 10:11:40 +0600

    Created makeSwitch Method

--- src/authenticationpkg/utilities/AuthenticationUtility.java
+++ src/authenticationpkg/utilities/AuthenticationUtility.java
@@ -1,9 +1,23 @@
 package authenticationpkg.utilities;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.text.Text;
+
 /**
  *
  * @author dewan
  */
 public class AuthenticationUtility {
+    
+    public static void makeSwitch(Text titleText, String newTitleText, Label retypePasswordLabel, boolean isRetypePasswordLabelVisible, PasswordField retypePasswordField, boolean isRetypePasswordFieldVisible, Button authButton, String newAuthButtonText, Label switchLabel, String newSwitchLabelText, Button switchButton, String newSwitchButtonText) {
+        titleText.setText(newTitleText);
+        retypePasswordLabel.setVisible(isRetypePasswordLabelVisible);
+        retypePasswordField.setVisible(isRetypePasswordFieldVisible);
+        authButton.setText(newAuthButtonText);
+        switchLabel.setText(newSwitchLabelText);
+        switchButton.setText(newSwitchButtonText);
+    }
     
 }
