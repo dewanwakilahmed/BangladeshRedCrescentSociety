1eb42324f7c2d68c369fdd4f9d9be41d1297f5b1
dewanwakilahmed <dewanwakilahmed@gmail.com>
2023-11-27 10:54:05 +0600

    Created User Model

---
+++ src/authenticationpkg/models/User.java
@@ -0,0 +1,42 @@
+package authenticationpkg.models;
+
+/**
+ *
+ * @author dewan
+ */
+public class User {
+    private String userName;
+    private String userPassword;
+    private String userRole;
+
+    public User(String userName, String userPassword, String userRole) {
+        this.userName = userName;
+        this.userPassword = userPassword;
+        this.userRole = userRole;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+   
+}
